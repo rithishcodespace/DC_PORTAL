74eb77b6672a808e84b1049e2a916dd9adf00949
rithishcodespace <rithishcodespace@gmail.com>
2025-08-01 13:42:14 +0530

    added schema

--- backend/app.js
+++ backend/app.js
@@ -31,4 +31,4 @@ app.use((error,req,res,next) => {
     })
 })
 
-app.listen(PORT, () => console.log("Server is running on port "+PORT));
+app.listen(PORT, () => console.log("Server is running on http://localhost:"+PORT));
